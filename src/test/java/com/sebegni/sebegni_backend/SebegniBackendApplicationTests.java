6d0ffb253960ef4459f31ad387e22671b0c22191
MohamedELO3ROI2O <23099@supnum.mr>
2026-04-22 21:11:22 +0000

    starter app

---
+++ src/test/java/com/sebegni/sebegni_backend/SebegniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebegni.sebegni_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebegniBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
